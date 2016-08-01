49fd17c00750e3324d3bc0f6098e233f19b41df2
gagato <SyrbagRo8in1>
2016-08-01 20:49:58 +0200

    adding mor initial stuff

--- pysoft/minimu9v5/gyro.py
+++ pysoft/minimu9v5/gyro.py
@@ -1,11 +1,24 @@
 import smbus
 import time
 
+CTRL10_C = 0x19
+CTRL10_C_SIGN_MOTION_EN = 0x01
+CTRL10_C_PEDO_RST_STEP = 0x02
+CTRL10_C_FUNC_EN = 0x04
+CTRL10_C_Xen_G = 0x08
+CTRL10_C_Yen_G = 0x10
+CTRL10_C_Zen_G = 0x20
+CTRL1_XL = 0x10
+CTRL1_XL_BW_XL0 = 0x01
+CTRL1_XL_BW_XL1 = 0x02
+CTRL1_XL_FS_XL0 = 0x04
+CTRL1_XL_FS_XL1 = 0x08
+CTRL1_XL_ODR_XL0 = 0x10
+CTRL1_XL_ODR_XL1 = 0x20
+CTRL1_XL_ODR_XL2 = 0x40
+CTRL1_XL_ODR_XL3 = 0x80
+
 class Gyro:
-    CTRL9_XL_ADDRESS = 0x18
-    CTRL9_XL_ENABLE_X = 0x80
-    CTRL9_XL_ENABLE_Y = 0x10
-    CTRL9_XL_ENABLE_Z = 0x10
 
     def __init__(self, bus_id = 2, gyro_address = 0x6b):
         self.bus_id = bus_id
@@ -13,8 +26,7 @@ class Gyro:
         self.bus = smbus.SMBus(2)
 
     def enable(self):
-        self.bus.write_byte_data(self.gyro_address, self.CTRL9_XL_ADDRESS, self.CTRL9_XL_ENABLE_X |
-                                 self.CTRL9_XL_ENABLE_Y | self.CTRL9_XL_ENABLE_Z)
+        self.bus.write_byte_data(self.gyro_address, CTRL10_C, CTRL10_C_Xen_G | CTRL10_C_Yen_G | CTRL10_C_Zen_G)
 
 if __name__ == "__main__":
     g = Gyro()
