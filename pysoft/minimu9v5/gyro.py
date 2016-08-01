b55ff4f760ad34425919e6118de02e4d89f6bcc6
gagato <SyrbagRo8in1>
2016-08-01 22:27:31 +0200

    adding mor initial stuff

--- pysoft/minimu9v5/gyro.py
+++ pysoft/minimu9v5/gyro.py
@@ -26,7 +26,12 @@ STATUS_REG_XLDA = 0x01
 STATUS_REG_GDA = 0x02
 STATUS_REG_TDA = 0x04
 STATUS_REG_EV_BOOT = 0x08
-
+OUTX_L_G = 0x22
+OUTX_H_G = 0x23
+OUTY_L_G = 0x24
+OUTY_H_G = 0x25
+OUTZ_L_G = 0x26
+OUTZ_H_G = 0x27
 
 class Gyro:
 
@@ -35,7 +40,7 @@ class Gyro:
         self.gyro_address = gyro_address
         self.bus = smbus.SMBus(2)
 
-    def device_enable(self):
+    def enable(self):
         self.bus.write_byte_data(self.gyro_address, CTRL10_C, CTRL10_C_Xen_G | CTRL10_C_Yen_G | CTRL10_C_Zen_G)
 
     def enable_high_performance_mode(self):
@@ -56,9 +61,31 @@ class Gyro:
     def is_data_ready(self):
         return self.get_data_ready_status() & STATUS_REG_GDA > 0
 
+    def binary_to_int(self, raw_value):
+        if (raw_value >= 0x8000):
+                int_val = -((65535 - raw_value) + 1)
+        else:
+                int_val = raw_value
+        return int_val
+
+    def get_X(self):
+        raw_data =  self.bus.read_word_data(self.gyro_address, OUTX_L_G)
+        return self.binary_to_int(raw_data)
+
+    def get_Y(self):
+        raw_data =  self.bus.read_word_data(self.gyro_address, OUTY_L_G)
+        return self.binary_to_int(raw_data)
+
+    def get_Z(self):
+        raw_data =  self.bus.read_word_data(self.gyro_address, OUTZ_L_G)
+        return self.binary_to_int(raw_data)
+
 if __name__ == "__main__":
     g = Gyro()
-    g.device_enable()
+    g.enable()
     g.enable_high_performance_mode()
     g.set_output_data_rate(CTRL2_G_13HZ)
     print(g.is_data_ready())
+    print(g.get_X())
+    print(g.get_Y())
+    print(g.get_Z())
