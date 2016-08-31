5d98ea5e0a97f08bf3ddf6ac41dafd739783565e
gagato <SyrbagRo8in1>
2016-08-31 22:11:26 +0200

    adding mor initial stuff

--- pysoft/minimu9v5/minimu.py
+++ pysoft/minimu9v5/minimu.py
@@ -43,47 +43,36 @@ class Minimu():
         self.fifo.set_gyro_decimation_factor(0)
         self.fifo.set_odr_hz(0)
 
+    def to_angle(self, sample_sum, sample_count):
+        if sample_sum >= 0:
+            return sample_sum * self.GYRO_POSITIVE_FACTOR + sample_count * self.GYRO_OFFSET
+        else:
+            return sample_sum * self.GYRO_NEGATIVE_FACTOR + sample_count * self.GYRO_OFFSET
+
     def read_gyro(self):
         data = self.fifo.get_data()
         print(data)
         if data:
-            self.angles['X'] += data[0][0]
-            self.angles['Y'] += data[1][0]
-            self.angles['Z'] += data[2][0]
-            return {'X': data[0][0], 'Y': data[1], 'Z': data[2][0]}
+            self.angles['X'] += self.to_angle(data[0][0], data[0][1])
+            self.angles['Y'] += self.to_angle(data[1][0], data[1][1])
+            self.angles['Z'] += self.to_angle(data[2][0], data[2][1])
+            return {'X': self.to_angle(data[0][0], data[0][1]),
+                    'Y': self.to_angle(data[1][0], data[1][1]),
+                    'Z': self.to_angle(data[2][0], data[2][1])}
         else:
             return dict(X=0, Y=0, Z=0)
-
-    # def to_angle(self, sample_sum, sample_count):
-    #     if sample_sum >= 0:
-    #         return sample_sum * self.GYRO_POSITIVE_FACTOR + sample_count * self.GYRO_OFFSET
-    #     else:
-    #         return sample_sum * self.GYRO_NEGATIVE_FACTOR + sample_count * self.GYRO_OFFSET
-
-    # def read_gyro(self):
-    #     data = self.fifo.get_data()
-    #     print(data)
-    #     if data:
-    #         self.angles['X'] += self.to_angle(data[0][0], data[0][1])
-    #         self.angles['Y'] += self.to_angle(data[1][0], data[1][1])
-    #         self.angles['Z'] += self.to_angle(data[2][0], data[2][1])
-    #         return {'X': self.to_angle(data[0][0], data[0][1]),
-    #                 'Y': self.to_angle(data[1][0], data[1][1]),
-    #                 'Z': self.to_angle(data[2][0], data[2][1])}
-    #     else:
-    #         return dict(X=0, Y=0, Z=0)
 
     def print_angles_degrees(self):
         print("Degrees: X: {0:.12f},  Y: {1:.12f}, Z:  {2:.12f}".format(self.angles['X'],
                                                                         self.angles['Y'], self.angles['Z']))
-    #
-    # def __degree_to_radian(self, degrees):
-    #     return degrees * 3.14159265/180
-    #
-    # def print_angles_radians(self):
-    #         print("Radians: X: {0:.12f},  Y: {1:.12f}, Z:  {2:.12f}".format(self.__degree_to_radian(self.angles['X']),
-    #                                                             self.__degree_to_radian(self.angles['Y']),
-    #                                                             self.__degree_to_radian(self.angles['Z'])))
+
+    def __degree_to_radian(self, degrees):
+        return degrees * 3.14159265/180
+
+    def print_angles_radians(self):
+            print("Radians: X: {0:.12f},  Y: {1:.12f}, Z:  {2:.12f}".format(self.__degree_to_radian(self.angles['X']),
+                                                                self.__degree_to_radian(self.angles['Y']),
+                                                                self.__degree_to_radian(self.angles['Z'])))
 
 
 if __name__ == "__main__":
@@ -97,9 +86,9 @@ if __name__ == "__main__":
     try:
         while 1:
             print("Last reading: {0}".format(mm.read_gyro()))
-            # mm.print_angles_degrees()
-            # mm.print_angles_radians()
-            time.sleep(0.5)
+            mm.print_angles_degrees()
+            mm.print_angles_radians()
+            time.sleep(1)
     except KeyboardInterrupt:
         mm.disable_fifo()
         #mm.disable_gyro()
