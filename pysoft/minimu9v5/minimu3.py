90e536bf7cc02fc0c8e67acea875db33846cc80d
gagato <SyrbagRo8in1>
2016-09-05 20:07:28 +0200

    Research

--- pysoft/minimu9v5/minimu3.py
+++ pysoft/minimu9v5/minimu3.py
@@ -24,6 +24,8 @@ class Minimu():
         self.acc_axes = 'YZ'
         self.acc_positive_factor = self.acc_full_scale / self.MAX_POSITIVE_16
         self.acc_negative_factor = self.acc_full_scale / self.MIN_NEGATIVE_16
+        self.fifo_pattern_size = 6
+        self.angles  = {'A':0, 'G':0, 'C':0}
 
     def setup_gyro(self):
         self.gyro.set_full_scale_selection(self.gyro_full_scale)
@@ -48,6 +50,8 @@ class Minimu():
         self.fifo.set_acc_decimation_factor(1)
         self.fifo.set_odr_hz(self.gyro_odr_hz)
         self.fifo.set_mode('Continuous')
+        time.sleep(0.2)
+        self.fifo.get_data(self.fifo_pattern_size)
 
     def disable_fifo(self):
         self.fifo.set_mode('Bypass')
@@ -62,14 +66,19 @@ class Minimu():
             return sample_sum * self.gyro_negative_factor
 
     def read(self):
-        fifo_pattern_size = 6
-        data = self.fifo.get_data(fifo_pattern_size)
+        data = self.fifo.get_data(self.fifo_pattern_size)
         radian_factor = 3.14159265359 / 180.0
         gyro_pitch = [d * self.gyro_positive_factor * radian_factor if d >= 0 else d * self.gyro_negative_factor * radian_factor for d in data[0]]
+        for g in gyro_pitch: self.angles['G'] += g * 1.0 / self.gyro_odr_hz
         acc_y = [d * self.acc_positive_factor if d >= 0 else d * self.acc_negative_factor for d in data[4]]
         acc_z = [d * self.acc_positive_factor if d >= 0 else d * self.acc_negative_factor for d in data[5]]
         acc_pitch = [math.atan2(y, z) for y, z in zip(acc_y, acc_z)]
-        return [(x, y) for x, y in zip(gyro_pitch, acc_pitch)]
+        for a in acc_pitch: self.angles['A'] += a
+
+        for g, a in zip(gyro_pitch, acc_pitch):
+            self.angles['C'] = 0.98*(self.angles['C'] + g * 1.0 / self.gyro_odr_hz) + 0.02 * a
+
+        return [(x, y) for x, y in zip(self.angles['A'], self.angles['B'], self.angles['C'])]
 
 
 if __name__ == "__main__":
