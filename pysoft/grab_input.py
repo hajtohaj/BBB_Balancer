3db95f5fd2a76e15b90df67804fe1e1324cb1d4c
gagato <SyrbagRo8in1>
2016-10-17 20:32:55 +0200

    Research

--- pysoft/grab_input.py
+++ pysoft/grab_input.py
@@ -1,3 +1,15 @@
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
 class _GetchUnix:
     def __init__(self):
         import tty, sys
@@ -13,8 +25,14 @@ class _GetchUnix:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
-if __name__ == "__main__":
 
-    c = 'a'
-    while c != 'e':
-        c = _GetchUnix()
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+
+getch = _Getch()
