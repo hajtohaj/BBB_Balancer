951fb0b4a1178c7d1488d7ea1ed1febb1576887a
gagato <SyrbagRo8in1>
2016-10-18 20:39:10 +0200

    Research

--- pysoft/motor/grab_input.py
+++ pysoft/motor/grab_input.py
@@ -65,14 +65,10 @@ while c != 'q' and c != 'Q':
                     print("left: {0}, right: {1}".format(m0.get_velocity(), m1.get_velocity()))
                 if key_map[ord(c)] == 'right':
                     m0_vel = m0.get_velocity()
-                    m1_vel = m1.get_velocity()
-                    m0.change_velocity(sign(m0_vel) * speed_step/2)
-                    m1.change_velocity(- sign(m1_vel) * speed_step/2)
+                    m0.change_velocity(sign(m0_vel) * speed_step)
                     print("left: {0}, right: {1}".format(m0.get_velocity(), m1.get_velocity()))
                 elif key_map[ord(c)] == 'left':
-                    m0_vel = m0.get_velocity()
                     m1_vel = m1.get_velocity()
-                    m0.change_velocity( - sign(m0_vel) * speed_step/2)
                     m1.change_velocity(sign(m1_vel) * speed_step/2)
                     print("left: {0}, right: {1}".format(m0.get_velocity(), m1.get_velocity()))
     elif ord(c) == 32:
