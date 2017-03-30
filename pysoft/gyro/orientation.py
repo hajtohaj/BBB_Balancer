f662b708de098a3ccb04dfe5e856e1ffdb0a91e2
gagato <SyrbagRo8in1>
2017-03-30 22:29:21 +0200

    Research

---
+++ pysoft/gyro/orientation.py
@@ -0,0 +1,18 @@
+from minimu import Minimu
+from time import sleep
+import numpy as np
+
+if __name__ == "__main__":
+    buss_id = 2
+    fifo_address = 0x6b
+
+    mm = Minimu(buss_id, fifo_address)
+    mm.enable()
+
+    try:
+        while 1:
+            print(mm.read())
+            sleep(1)
+
+    except KeyboardInterrupt:
+        mm.disable()
