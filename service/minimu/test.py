fe2964505399bbc8baa994d6e2dd3069371566c3
gagato <SyrbagRo8in1>
2017-04-22 15:26:46 +0200

    commit

--- service/minimu/test.py
+++ service/minimu/test.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     try:
         while 1:
             n = mm.read_fifo()
-            print(np.average(n[-9:][-3:], 2))
+            print(np.average(n[-9:][-3:], 1))
             sleep(0.1)
 
     except KeyboardInterrupt:
