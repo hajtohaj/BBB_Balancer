9f67aa0fcc892c2bfdd7a024e526ae3ea93af299
gagato <SyrbagRo8in1>
2017-04-22 16:38:13 +0200

    commit

--- service/minimu/test.py
+++ service/minimu/test.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
         while 1:
             acc_data = mm.read_fifo()[-av_length:, -3:]
             acc_data_av = np.average(acc_data, 0)
-            print(np.arctan2(acc_data_av[2], acc_data_av[1]))  # fi = np.arctan2(y, z)
+            #print(np.arctan2(acc_data_av[2], acc_data_av[1]))  # fi = np.arctan2(y, z)
+            print(acc_data_av)
             sleep(0.1)
 
     except KeyboardInterrupt:
