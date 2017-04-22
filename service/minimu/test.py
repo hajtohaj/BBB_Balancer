4bf906ea1400755fb1bde822f3370afc4166f236
gagato <SyrbagRo8in1>
2017-04-22 15:23:47 +0200

    commit

---
+++ service/minimu/test.py
@@ -0,0 +1,20 @@
+from minimu import Minimu
+from time import sleep
+import numpy as np
+
+
+if __name__ == "__main__":
+    buss_id = 2
+    fifo_address = 0x6b
+
+    mm = Minimu(buss_id, fifo_address)
+    mm.enable(104)
+
+    try:
+        while 1:
+            n = mm.read_fifo()
+            print(np.average(n[-9:][-3:]))
+            sleep(0.1)
+
+    except KeyboardInterrupt:
+        mm.disable()
