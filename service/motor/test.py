92bbc9ea209d14316289558d80d1dbcc2700b6e4
gagato <SyrbagRo8in1>
2017-04-22 18:58:08 +0200

    commit

--- service/motor/test.py
+++ service/motor/test.py
@@ -3,7 +3,7 @@ import time
 
 if __name__ == "__main__":
 
-    speed = 30
+    speed = -10
 
     m0 = Motor(0)
     m1 = Motor(1)
@@ -12,10 +12,10 @@ if __name__ == "__main__":
         while 1:
             print(m0.get_position(), m1.get_position())
             time.sleep(0.25)
-            m0.set_direction('cw')
-            m1.set_direction('ccw')
-            m0.set_speed(speed)
-            m1.set_speed(speed)
+            # m0.set_direction('cw')
+            # m1.set_direction('ccw')
+            m0.set_velocity(speed)
+            m1.set_velocity(speed)
 
     except KeyboardInterrupt:
         m0.close()
