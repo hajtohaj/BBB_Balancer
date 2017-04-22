b1094b7dc7cc14738544f7e57b605e234d90e391
gagato <SyrbagRo8in1>
2017-04-22 18:33:58 +0200

    commit

--- service/motor/test.py
+++ service/motor/test.py
@@ -3,9 +3,15 @@ import time
 
 if __name__ == "__main__":
 
+    speed = 10
+
     m0 = Motor(0)
     m1 = Motor(1)
 
     while 1:
         print(m0.get_position(), m1.get_position())
         time.sleep(0.25)
+        m0.set_direction('cw')
+        m1.set_direction('cw')
+        m0.set_voltage_level(speed)
+        m1.set_voltage_level(speed)
