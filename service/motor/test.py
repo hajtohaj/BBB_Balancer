005f7584a2e1c5af976f99de315465cb56a6ddb4
gagato <SyrbagRo8in1>
2017-04-22 18:41:10 +0200

    commit

--- service/motor/test.py
+++ service/motor/test.py
@@ -8,10 +8,15 @@ if __name__ == "__main__":
     m0 = Motor(0)
     m1 = Motor(1)
 
-    while 1:
-        print(m0.get_position(), m1.get_position())
-        time.sleep(0.25)
-        m0.set_direction('cw')
-        m1.set_direction('cw')
-        m0.set_speed(speed)
-        m1.set_speed(speed)
+    try:
+        while 1:
+            print(m0.get_position(), m1.get_position())
+            time.sleep(0.25)
+            m0.set_direction('cw')
+            m1.set_direction('cw')
+            m0.set_speed(speed)
+            m1.set_speed(speed)
+
+    except KeyboardInterrupt:
+        m0.close()
+        m0.close()
