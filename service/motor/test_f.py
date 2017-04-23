29229388399365235282852b98a2b5208beeb200
gagato <SyrbagRo8in1>
2017-04-23 19:05:48 +0200

    commit

--- service/motor/test_f.py
+++ service/motor/test_f.py
@@ -1,19 +1,20 @@
-from motor import Motor
+#!/usr/bin/python3
+from motor2 import Motor
 import time
 
 if __name__ == "__main__":
 
-    speed = 15
+    voltage_level = -15
 
     m0 = Motor(0)
     m1 = Motor(1,-1)
 
     try:
         while 1:
-            print(m0.get_position(), m1.get_position())
+            print(m0.get_radians(), m1.get_radians())
+            m0.set_voltage(voltage_level)
+            m1.set_voltage(voltage_level)
             time.sleep(0.25)
-            m0.set_velocity(speed)
-            m1.set_velocity(speed)
 
     except KeyboardInterrupt:
         m0.close()
