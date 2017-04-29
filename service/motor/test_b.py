e0ea609f98dad3ce44cf51943b86bd67fc85f0f4
gagato <SyrbagRo8in1>
2017-04-29 20:07:20 +0200

    commit

--- service/motor/test_b.py
+++ service/motor/test_b.py
@@ -5,26 +5,30 @@ import time
 
 if __name__ == "__main__":
 
-    voltage_level = 15
-    delta_t = 0.1
+    voltage_level = 10
+    step_size = 0.1
 
     m0 = Motor(0)
-    pid = PID(20.0, 0.5, 0.0, 0.1)
+    pid = PID(5.0, 200.0, 0.0, 0.1)
 
+    t0 = time.time()
     r0 = m0.get_radians()
 
     try:
         while 1:
+            t1 = time.time()
+            r1 = m0.get_radians()
+
+            dt = (t1-t0) % step_size
+            dr = r1-r0
+            v = dr / dt
+            u = pid.step(v)
+
+            m0.set_voltage(u)
+            r0 = m0.get_radians()
             t0 = time.time()
 
-            r1 = m0.get_radians()
-            print(r0)
+            time.sleep(step_size - dt)
 
-            m0.set_voltage(voltage_level)
-
-            t1 = time.time()
-            time.sleep(delta_t - (t1 - t0) % delta_t)
-            print(time.time())
-            r1 = m0.get_radians()
     except KeyboardInterrupt:
         m0.close()
