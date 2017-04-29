ed7245ff0b5d6889528cef243de252fe1f52a154
gagato <SyrbagRo8in1>
2017-04-29 20:52:42 +0200

    commit

--- service/motor/test_b.py
+++ service/motor/test_b.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     step_size = 0.5
 
     m0 = Motor(0)
-    pid = PID(1.0, 10.0, 0.0, 0.1)
+    pid = PID(10.0, 100.0, 0.0, 0.1)
 
     t0 = time.time()
     r0 = m0.get_radians()
